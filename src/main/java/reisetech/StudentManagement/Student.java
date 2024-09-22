ffee0a8557bd1ad6d9e61aa1a1ccb8ff3f0f7d8c
Anru Iwase <iwaan454@gmail.com>
2024-09-22 15:14:25 +0900

    Javaスタンダード第10èª回課題作成

--- src/main/java/reisetech/StudentManagement/Student.java
+++ src/main/java/reisetech/StudentManagement/Student.java
@@ -1,22 +1,19 @@
 package reisetech.StudentManagement;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Student {
+
+  private String id;
   private String name;
+  private String furigana;
+  private String nickname;
+  private String email;
+  private String address;
   private int age;
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
+  public String gender;
 }
