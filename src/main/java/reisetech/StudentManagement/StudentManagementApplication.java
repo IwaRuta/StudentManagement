848f366bd587e254b9968d1f66415bec5cdb88c5
Anru Iwase <iwaan454@gmail.com>
2024-09-23 14:52:14 +0900

    StudentServiceクラスにて、30ä»30ï代ãのみとJava スタンダードãのみãの抽出å®を実装しました。

--- src/main/java/reisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/reisetech/StudentManagement/StudentManagementApplication.java
@@ -1,14 +1,8 @@
 package reisetech.StudentManagement;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import reisetech.StudentManagement.StudentRepository.StudentRepository;
-import reisetech.StudentManagement.data.Student;
-import reisetech.StudentManagement.data.StudentCourse;
 
 @SpringBootApplication
 
--- src/main/java/reisetech/StudentManagement/service/StudentService.java
+++ src/main/java/reisetech/StudentManagement/service/StudentService.java
@@ -1,6 +1,8 @@
 package reisetech.StudentManagement.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reisetech.StudentManagement.StudentRepository.StudentRepository;
@@ -17,11 +19,35 @@ public class StudentService {
     this.repository = repository;
   }
 
-  public List<Student> searchStudentList(){
-    return repository.searchStudent();
+  public List<Student> searchStudentList() {
+
+    /**
+     * 30代のみ抽出実装
+     */
+
+    List<Student> students = repository.searchStudent();
+    List<Student> thirtiesStudents = students.stream()
+        .filter(student -> student.getAge() >= 30 && student.getAge() < 40)
+        .collect(Collectors.toList());
+
+    return thirtiesStudents;
   }
 
-  public List<StudentCourse>searchStudentCourseList(){
-    return repository.searchStudentCourse();
+  /**
+   * "Javaスタンダード"のみ抽出実装。
+   */
+
+  public List<StudentCourse> searchStudentCourseList() {
+    List<StudentCourse> studentCourses = repository.searchStudentCourse();
+    List<StudentCourse> javaStandardStudents = new ArrayList<>();
+
+    for (StudentCourse studentCourse : studentCourses) {
+      if ("Javaスタンダード".equals(studentCourse.getCourseName())) {
+        javaStandardStudents.add(studentCourse);
+      }
+    }
+    return javaStandardStudents;
   }
 }
+
+
