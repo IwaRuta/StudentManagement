3065f75b8c57a34b725ec3d7316658b07a9aee12
Anru Iwase <iwaan454@gmail.com>
2024-12-08 14:14:50 +0900

    OpenAPI仕様書の導入実装

--- src/main/java/reisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/reisetech/StudentManagement/controller/StudentController.java
@@ -1,5 +1,6 @@
 package reisetech.StudentManagement.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import java.util.List;
@@ -37,6 +38,7 @@ public class StudentController {
    * @return　受講生詳細一覧(全件)
    */
 
+  @Operation(summary = "一覧検索", description = "受講生の一覧を検索します。")
   @GetMapping("/studentList")
   public List<StudentDetail> getStudentList() {
     return service.searchStudentList();
@@ -61,6 +63,7 @@ public class StudentController {
    * @return　実行結果
    */
 
+  @Operation(summary = "受講生登録",description = "新規受講生を登録します。")
   @PostMapping("/registerStudent")
   public ResponseEntity<StudentDetail> registerStudent(
       @RequestBody @Valid StudentDetail studentDetail) {
