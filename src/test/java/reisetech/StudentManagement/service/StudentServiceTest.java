21e9090cb5b5df3a87e9a341edb5ce57f92d3d40
Anru Iwase <iwaan454@gmail.com>
2025-01-11 14:57:49 +0900

    Service正常テスト修正

--- src/test/java/reisetech/StudentManagement/service/StudentServiceTest.java
+++ src/test/java/reisetech/StudentManagement/service/StudentServiceTest.java
@@ -1,9 +1,11 @@
 package reisetech.StudentManagement.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -48,22 +50,6 @@ class StudentServiceTest {
   }
 
   @Test
-  void 受講生詳細一覧検索_リストに一件の情報が入っている場合の異常テスト() {
-    List<Student> studentList = new ArrayList<>();
-    List<StudentCourse> studentCourseList = new ArrayList<>();
-    when(repository.search()).thenReturn(studentList);
-    when(repository.searchStudentCourseList()).thenReturn(studentCourseList);
-
-    sut.searchStudentList();
-
-    List<Student> searchTest = List.of(new Student());
-
-    verify(repository, times(1)).search();
-    verify(repository, times(1)).searchStudentCourseList();
-    verify(converter, times(1)).convertStudentDetails(searchTest, studentCourseList);
-  }
-
-  @Test
   void 受講生詳細検索_リポジトリから受講生IDに基づいたデータが取得できること() {
     String id = "123";
     Student student = new Student();
@@ -77,25 +63,8 @@ class StudentServiceTest {
 
     verify(repository, times(1)).searchStudent(id);
     verify(repository, times(1)).searchStudentCourse(id);
+    assertEquals(expected.getStudent().getId(),actual.getStudent().getId());
   }
-
-  @Test
-  void 受講生詳細検索_受講生IDとは関係ない受講生の氏名も取得している場合の異常テスト() {
-    String id = "123";
-    Student student = new Student();
-    student.setId(id);
-
-    when(repository.searchStudent(id)).thenReturn(student);
-    when(repository.searchStudentCourse(id)).thenReturn(new ArrayList<>());
-
-    StudentDetail expected = new StudentDetail(student, new ArrayList<>());
-    StudentDetail actual = sut.searchStudent(id);
-
-    verify(repository, times(1)).searchStudent(id);
-    verify(repository, times(1)).searchStudentCourse(id);
-    verify(repository, times(1)).searchStudent(student.getName());
-  }
-
 
   @Test
   void 受講生新規登録_リポジトリの処理が適切に呼び出せていること() {
@@ -108,19 +77,6 @@ class StudentServiceTest {
 
     verify(repository, times(1)).registerStudent(student);
     verify(repository, times(1)).registerStudentCourse(studentCourse);
-  }
-
-  @Test
-  void 受講生新規登録_受講生コース情報が２回呼び出されることを期待している場合の異常テスト() {
-    Student student = new Student();
-    StudentCourse studentCourse = new StudentCourse();
-    List<StudentCourse> studentCourseList = List.of(studentCourse);
-    StudentDetail studentDetail = new StudentDetail(student, studentCourseList);
-
-    sut.registerStudent(studentDetail);
-
-    verify(repository, times(1)).registerStudent(student);
-    verify(repository, times(2)).registerStudentCourse(studentCourse);
   }
 
   @Test
@@ -137,16 +93,16 @@ class StudentServiceTest {
   }
 
   @Test
-  void 受講生更新_更新処理に不必要な登録処理を呼び出すことを期待している場合の異常テスト() {
+  void 受講生詳細の登録_初期化処理が動作すること() {
+    String id = "999";
     Student student = new Student();
+    student.setId(id);
     StudentCourse studentCourse = new StudentCourse();
-    List<StudentCourse> studentCourseList = List.of(studentCourse);
-    StudentDetail studentDetail = new StudentDetail(student, studentCourseList);
 
-    sut.updateStudent(studentDetail);
+    sut.initStudentCourse(studentCourse, student.getId());
 
-    verify(repository, times(1)).updateStudent(student);
-    verify(repository, times(1)).updateStudentCourse(studentCourse);
-    verify(repository, times(1)).registerStudent(student);
+   assertEquals("999", studentCourse.getStudentId());
+    assertEquals(LocalDateTime.now().getHour(), studentCourse.getStartDate().getHour());
+    assertEquals(LocalDateTime.now().plusYears(1).getYear(), studentCourse.getEndDate().getYear());
   }
 }
