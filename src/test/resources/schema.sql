2960ce89604efdc0384c514fa6e53ad61b5b273a
Anru Iwase <iwaan454@gmail.com>
2025-03-26 19:07:36 +0900

    data,schemaへStudentCourseへ申込状況（Status）の追加を行いました。

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -19,5 +19,5 @@ CREATE TABLE IF NOT EXISTS students_courses
   course_name VARCHAR(50) NOT NULL,
   start_date TIMESTAMP,
   end_date TIMESTAMP,
-  status ENUM("仮申込","本申込","受講中","受講終了"),NOT NULL
+  status VARCHAR(50) NOT NULL
 );
